793decb614faec1197c5a1e7e608e2ef247f9c94
ndiaye_b <ndiaye_b@etna-alternance.net>
2019-04-07 12:41:52 +0200

    modification des vues

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Bienvenue dans samaJob
                 </div>
 
                 <div class="links">
